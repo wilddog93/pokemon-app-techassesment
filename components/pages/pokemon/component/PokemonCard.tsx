79c48e60d79642aa11bd26c8937fac4a5363ff1f
wilddog93 <ridhoajibx@gmail.com>
2024-05-28 20:29:43 +0700

    [ridho] - delete theme switch

--- components/pages/pokemon/component/PokemonCard.tsx
+++ components/pages/pokemon/component/PokemonCard.tsx
@@ -18,8 +18,8 @@ const PokemonCard: React.FC<Props> = ({ id, carts, key }) => {
 
   if (isLoading) return (
     <div className="flex items-center gap-2 p-2 rounded-2xl bg-white text-default-600">
-      <Skeleton className="rounded-lg">
-        <div className="h-16 rounded-lg bg-gray-300">loading...</div>
+      <Skeleton className="w-full rounded-lg">
+        <div className="size-16 rounded-lg bg-gray-300">loading...</div>
       </Skeleton>
       <div className="w-full flex flex-col gap-1">
         <Skeleton className="w-full rounded-lg">
