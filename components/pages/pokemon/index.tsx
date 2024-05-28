68a45a984ca4f8251233954c40e733a93a934c7a
wilddog93 <ridhoajibx@gmail.com>
2024-05-29 03:07:21 +0700

    [ridho] - refactor loading

--- components/pages/pokemon/index.tsx
+++ components/pages/pokemon/index.tsx
@@ -17,6 +17,7 @@ import { usePokemons } from "@/utils/hooks/pokemon";
 import { Spinner } from "@nextui-org/spinner";
 import { Header } from "../Header";
 import { FormValues } from "./component/FormPokemon";
+import { Skeleton } from "@nextui-org/skeleton";
 
 const options = [
   {
@@ -84,21 +85,36 @@ export default function PokemonPage() {
   return (
     <main className="container mx-auto max-w-full px-8 flex-grow">
       <section className="w-full flex flex-col gap-6 py-8 tracking-wider">
-        <Header />
+        {isLoading ?
+          <div className='w-full flex items-center justify-between'>
+            <Skeleton className="rounded-lg">
+              <div className="size-12 rounded-lg bg-gray-300">loading...</div>
+            </Skeleton>
+            <Skeleton className="rounded-lg">
+              <div className="size-12 rounded-lg bg-gray-300">loading...</div>
+            </Skeleton>
+          </div>
+          :
+          <Header />
+        }
 
-        <h1 className="text-2xl sm:text-4xl font-extrabold">Pokemon</h1>
+        {isLoading ?
+          <Skeleton className="w-36 rounded-lg">
+            <div className="size-6 rounded-lg bg-gray-300">loading...</div>
+          </Skeleton> :
+          <h1 className="text-2xl sm:text-4xl font-extrabold">Pokemon</h1>}
 
         <div className={clsx(
           "flex items-center justify-center gap-1 text-lg font-bold text-center",
           isLoading ? "" : "hidden"
-          )}>
+        )}>
           <Spinner size="lg" label="loading..." />
         </div>
 
         <div className={clsx(
           "flex items-center justify-center gap-1 text-lg font-bold text-center",
           error ? "" : "hidden"
-          )}>
+        )}>
           Pokemon not found!
         </div>
 
