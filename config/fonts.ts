522819fdda2018b54041c54cb8675242b34e6ab0
wilddog93 <ridhoajibx@gmail.com>
2024-05-29 06:02:59 +0700

    [ridho] - change font sans as Rubik

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,4 +1,4 @@
-import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
+import { Fira_Code as FontMono, Rubik as FontSans } from "next/font/google";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
